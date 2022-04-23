ab0c7e410bfb813b6e28a08442600c284db3a784
Bryan Kim <bkim0826@gmail.com>
2022-04-23 17:37:02 -0400

    yeeah

--- client/App.jsx
+++ client/App.jsx
@@ -1,8 +1,11 @@
 import React, {useState, useEffect} from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Signup from './components/Signup.js'
 import Login from './components/Login.js'
 import Progressbar from './components/Progressbar.js'
 import AddApplication from './components/AddApplication.js'
+import Jobs from './components/Jobs.js';
+import Dashboard from './components/Dashboard.js';
 
 
 
@@ -10,10 +13,16 @@ import AddApplication from './components/AddApplication.js'
 function App(){
     return(
       <div>
-        <AddApplication></AddApplication>
-        <Progressbar></Progressbar>
-        <Signup></Signup>
-        <Login></Login>
+        <BrowserRouter>
+        <Routes>
+          <Route path='/signup' element={<Signup />}/>
+          <Route path='/login' element={<Login />}/>
+          <Route path='/applications' element={<AddApplication />}/>
+          <Route path='/dashboard' element={<Dashboard/>}/>
+          <Route path='/jobs' element={<Jobs />} />
+        </Routes>
+        </BrowserRouter>
+        
       </div>
     );
 }
--- client/components/Dashboard.js
+++ client/components/Dashboard.js
@@ -1,10 +1,32 @@
 import React, {useState, useEffect} from 'react';
+import CardApplication from './CardApplication';
 
+// /applications/:userId
 function Dashboard(){
+    const [applications, setApplications] = useState([])
+    useEffect(() => {
+    
+        fetch(`/api/applications/${localStorage.getItem("userId")}`)
+        .then((data)=> data.json())
+        .then(res=> setApplications(res))
+   
+  
+}, [])
 
-
+    function displayApplications(){
+        
+    }
+    console.log(applications)
+    return(
+        <div>
+            Hi,
+            {applications.length>0 && applications.map((app) => {return (<CardApplication {...app} key={app.id} />)})}
+            {/* <button onClick={() => getApplications()}>Testing</button> */}
+        </div>
+    )
 
 
     
 }
 
+export default Dashboard;
