6d1a12cb89a9c06199879f5cea18567817262510
Jessica Lee <hyojin.jessica.lee@gmail.com>
2022-04-25 12:48:08 -0700

    finish adding styles

--- client/App.jsx
+++ client/App.jsx
@@ -1,29 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Signup from './components/Signup.js';
-import Login from './components/Login.js';
-import Progressbar from './components/Progressbar.js';
-import AddApplication from './components/AddApplication.js';
-import Jobs from './components/Jobs.js';
+
+import JobDashboard from './components/JobDashboard.js';
 import Dashboard from './components/Dashboard.js';
 import LoginTemplate from './components/LoginTemplate';
 import SignUpTemplate from './components/SignUpTemplate.js';
 import ApplicationTemplate from './components/ApplicationTemplate';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<LoginTemplate />} />
-          <Route path='/signup' element={<SignUpTemplate />} />
-
-          <Route path='/applications' element={<ApplicationTemplate />} />
-          <Route path='/dashboard' element={<Dashboard />} />
-          <Route path='/jobs' element={<Jobs />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<LoginTemplate />} />
+        <Route path='/signup' element={<SignUpTemplate />} />
+        <Route path='/applications' element={<ApplicationTemplate />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/jobs' element={<JobDashboard />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ client/components/Navbar.js
@@ -0,0 +1,68 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import { connect } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+import { clientSignout } from '../actions/actions';
+
+function NavBar({ loggedIn, clientSignout }) {
+  const navigate = useNavigate();
+  const handleSignout = () => {
+    localStorage.clear();
+    clientSignout();
+    navigate('/');
+  };
+  const actions = () => {
+    if (!loggedIn) {
+      return (
+        <>
+          <Link to='/' style={{ textDecoration: 'none', color: 'white' }}>
+            <Button color='inherit'>Login</Button>
+          </Link>
+          <Link to='/signup' style={{ textDecoration: 'none', color: 'white' }}>
+            <Button color='inherit'>Signup</Button>
+          </Link>
+        </>
+      );
+    } else {
+      return (
+        <Button color='inherit' onClick={handleSignout}>
+          Signout
+        </Button>
+      );
+    }
+  };
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position='static'>
+        <Toolbar>
+          <IconButton
+            size='large'
+            edge='start'
+            color='inherit'
+            aria-label='menu'
+            sx={{ mr: 2 }}
+          ></IconButton>
+
+          <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
+            <Link to='/' style={{ textDecoration: 'none', color: 'white' }}>
+              {' '}
+              Wunderpuss{' '}
+            </Link>
+          </Typography>
+
+          {actions()}
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
+const mapStateToProps = ({ loginStatus }) => {
+  return loginStatus;
+};
+
+export default connect(mapStateToProps, { clientSignout })(NavBar);
